2ce43dabd5d53dbaa244bcf0335ebd21ed084056
ecasada <ezequiel.casada2@globallogic.com>
2025-05-16 19:18:39 -0300

    add: Clase Cuenta

---
+++ src/main/java/com/example/banco/model/Cuenta.java
@@ -0,0 +1,28 @@
+package com.example.banco.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo", discriminatorType = DiscriminatorType.STRING)
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor // Constructor con todos los campos
+public abstract class Cuenta {
+    @Id
+    private String numero;
+    private String titular;
+    private double saldo;
+
+    public abstract void retirar(double monto);
+
+    public void depositar(double monto) {
+        if (monto > 0) {
+            this.saldo += monto;
+        }
+    }
+}
